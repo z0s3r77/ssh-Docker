f4f8d036e2687244a882a014a509db9281dcc540
Sebastián Estacio R <80277545+z0s3r77@users.noreply.github.com>
2023-02-10 12:46:02 +0100

    Update dockerScript.sh

--- dockerScript.sh
+++ dockerScript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#Comprobación de que el usuario es root
+clear
 echo "----------------------------------";
 echo "Checking if it's Docker installed on the LocalHost...";
 echo "----------------------------------";
 if [ $(whoami) != "root" ]; then
-
 
 	echo " -------------------------------- ";
 	echo "You must to be ROOT";
@@ -19,9 +20,10 @@ else
 
 
 fi
-
 sleep 2
 
+
+#Compración de que Docker está instalado
 if [ $(command -v docker) ]
 then
 	echo " ";
@@ -49,13 +51,20 @@ else
 		echo " ";
 		exit
 	fi
-    
 fi
+
+
+
+
+# A continuación pregunta si hay un contenedor iniciado con el puerto 22 abierto
+####### En caso de haberlo, lista los contenedores, el usuario selecciona un contenedor y introduce un usuario y contraseña.
+####### A continuación el script instala todo el arsenal y por último pregunta si el usuario quiere entrar o no al contenedor.
 echo " ";
 echo "------------------------------";
 read -p "Do you have a container running with port 22 openned?(y/n)" answer
 echo "------------------------------";
 echo " ";
+
 if [ $answer = "y" ]
 then
 	echo "Listing contairners with port 22 opened.....";
@@ -82,26 +91,35 @@ then
 	echo "--------------------------------- "
 	echo "Adding user and password to container";
 	docker exec -it $container adduser $user 
-	echo " "
-	echo " "
+	clear
+	echo "--------------------------------";
 	echo "Starting openssh-server......"
 	docker exec -it $container service ssh start 1>/dev/null
 	echo "--------------------------------";
 	read -p "Do you want to connect to the container?(y/n): " answ
 
-if [ $answ = "y" ]
-then
-	ip=$(docker inspect --format {{".NetworkSettings.IPAddress"}} $container)
-	ssh $user@$ip
-else
-	echo "  ";
-	echo "--------------------------------";
-	echo "openssh-server is now installed at the container $container , with the user $user, don't forget your password ;-)";
-	echo "--------------------------------";
-	echo " ";
-fi
+	if [ $answ = "y" ]
+	then
+		ip=$(docker inspect --format {{".NetworkSettings.IPAddress"}} $container)
+		ssh-keygen -f "/root/.ssh/known_hosts" -R "$ip"
+		ssh $user@$ip
+	else
+		echo "  ";
+		echo "--------------------------------";
+		echo "openssh-server is now installed at the container $container , with the user $user, don't forget your password ;-)";
+		echo "--------------------------------";
+		echo " ";
+	fi
 
 else
+
+
+
+
+
+# Este es el caso en el que usuario no tiene un contenedor con el puerto 22 abierto:
+##### Se le pregunta, si quiere abrir el puerto 22 en un contenedor existente o si quiere crear un contenedor desde 0
+
 	echo " ";
 	clear
 	echo " ";
@@ -112,8 +130,57 @@ else
 
 case $answ in
 "1")
-	echo "Opción 1";
+	
+	echo "Give me the following data about the container:"
+        read -p "Container Name: " name
+        read -p "User for ssh: " user
+        read -p "Password for ssh: " password
+	echo "You must to write 'exit'";
+	#Cambiar el puerto cada vez que se ejecute el script
+	docker run -i -p 32771:22 --name $name ubuntu
+	docker start $name 
+
+	echo "--------------------------------- ";
+        echo "Installing openssh-server at the container";
+        echo " ";
+        docker exec -it $name apt-get update -y 1>/dev/null
+        echo  ' #######                   (25%)\r'
+        docker exec -it $name apt-get install openssh-server -y 1>/dev/null
+        echo  ' ############              (50%)\r'
+        docker exec -it $name apt-get install systemctl -y 1>/dev/null
+        echo  ' ###################       (75%)\r'
+        docker exec -it $name systemctl status ssh 1>/dev/null
+        echo  ' ##########################(100%)\r'
+        sleep 1
+        echo "--------------------------------- ";
+        echo "Adding user and password to container";
+        docker exec -it $name adduser $user
+        echo " "
+        echo " "
+        echo "Starting openssh-server......"
+        docker exec -it $name service ssh start 1>/dev/null
+        echo "--------------------------------- ";
+        read -p "Do you want to connect to the container?(y/n): " answ
+
+
+	if [ $answ = "y" ]
+	then
+        	ip=$(docker inspect --format {{".NetworkSettings.IPAddress"}} $name)
+		ssh-keygen -f "/root/.ssh/known_hosts" -R "$ip"
+        	ssh $user@$ip
+	else
+        	echo "  ";
+        	echo "--------------------------------";
+        	echo "openssh-server is now installed at the container $name , with the user $user, don't forget your password ;-)";
+        	echo "--------------------------------";
+        	echo " ";
+	fi
 ;;
+
+
+
+#En este caso, se seleccionará un contenedor cualquiera y se le expondrá el puerto 22.
+#Importante mencionar que durante el proceso, el servicio de Docker se detendrá.
 "2")
 	echo " " ;
 	echo "--------------------------------";
@@ -130,10 +197,10 @@ case $answ in
         read -p "Password for ssh: " password
 	read -p "Port from host: " port
         echo "--------------------------------";
-	echo " ME explotó la cabeza ";
+
 	containerID=$(docker inspect --format {{".Id"}} $container)
 
-#	echo "$containerID";
+
 
 	systemctl stop docker.socket
 	systemctl stop docker
@@ -152,7 +219,6 @@ case $answ in
 	
 	docker start $containerID
 
-	### docker run -it --name unsimplecontendor -p 4324:234 ubuntu
 
 	echo "---------------------------------";
 	echo " ";
@@ -180,21 +246,18 @@ case $answ in
 
 
 	if [ $answ = "y" ]
-then
-        ip=$(docker inspect --format {{".NetworkSettings.IPAddress"}} $container)
+	then
+        	ip=$(docker inspect --format {{".NetworkSettings.IPAddress"}} $container)
     
-	  ssh-keygen -f "/root/.ssh/known_hosts" -R "$ip"
-
-    	ssh $user@$ip
-else
+		ssh-keygen -f "/root/.ssh/known_hosts" -R "$ip"
+    		ssh $user@$ip
+	else
         echo "  ";
         echo "--------------------------------";
         echo "openssh-server is now installed at the container $container , with the user $user, don't forget your password ;-)";
         echo "--------------------------------";
         echo " ";
-fi
-
-
+	fi	
 
 ;;
 esac
